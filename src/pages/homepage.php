b7ea7927e85b023b7d61d7818cd4b3db12e5dc89
Wouter van den Brink <wouter@woutervdb.com>
2013-07-28 23:56:25 +0200

    Fix typo
    I hope it's a typo.

--- src/pages/homepage.php
+++ src/pages/homepage.php
@@ -6,7 +6,7 @@
 ?>
 <h3>Hallo!</h3>
 
-Deze website is bedoelt voor leden van <a href="http://www.game-maker.nl">www.game-maker.nl</a>.
+Deze website is bedoeld voor leden van <a href="http://www.game-maker.nl">www.game-maker.nl</a>.
 Je kunt hier profielen bekijken van mensen om teamgenoten te zoeken, en natuurlijk je eigen profiel maken. Wat wil je doen?<br/>
 <br/>
 <a href="./?page=profielen">Profielen bekijken</a><br/>
